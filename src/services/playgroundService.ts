67bafa819e6d1bb20aafe11cc28aa6755545448c
Annie Kuxa <kuxa.business@gmail.com>
2023-06-09 09:39:31 -0300

    refactor port

--- src/services/playgroundService.ts
+++ src/services/playgroundService.ts
@@ -8,7 +8,7 @@ export const createPlayground = async (playgroundData: any, token: string) => {
     },
   }
 
-  const res = await axios.post('http://localhost:3001/api/playgrounds', playgroundData, config)
+  const res = await axios.post('http://joybox-new.fly.dev/api/playgrounds', playgroundData, config)
   return res.data
 }
 
@@ -20,7 +20,7 @@ export const getAllPlaygrounds = async (token: string) => {
     },
   }
 
-  const res = await axios.get('http://localhost:3001/api/playgrounds', config)
+  const res = await axios.get('http://joybox-new.fly.dev/api/playgrounds', config)
   return res.data
 }
 
@@ -32,7 +32,7 @@ export const getPlayground = async (playgroundId: string, token: string) => {
     },
   }
 
-  const res = await axios.get(`http://localhost:3001/api/playgrounds/${playgroundId}`, config)
+  const res = await axios.get(`http://joybox-new.fly.dev/api/playgrounds/${playgroundId}`, config)
   return res.data
 }
 
@@ -44,7 +44,7 @@ export const updatePlayground = async (playgroundId: string, updatedPlayground: 
     },
   }
 
-  const res = await axios.put(`http://localhost:3001/api/playgrounds/${playgroundId}`, updatedPlayground, config)
+  const res = await axios.put(`http://joybox-new.fly.dev/api/playgrounds/${playgroundId}`, updatedPlayground, config)
   return res.data
 }
 
@@ -56,7 +56,7 @@ export const deletePlayground = async (playgroundId: string, token: string) => {
     },
   }
 
-  const res = await axios.delete(`http://localhost:3001/api/playgrounds/${playgroundId}`, config)
+  const res = await axios.delete(`http://joybox-new.fly.dev/api/playgrounds/${playgroundId}`, config)
   return res.data
 }
 
